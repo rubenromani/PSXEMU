edc426e5e5c20410fe2164c3add571d74a987da8
rubenromani <ruben.romani@outlook.it>
2023-09-07 20:43:30 +0200

    Add project files.

---
+++ PSXEMU_GTest/test.cpp
@@ -0,0 +1,13 @@
+#include "pch.h"
+
+GTEST_API_ int main(int argc, char** argv)
+{
+	printf("Running main()");
+	testing::InitGoogleTest(&argc, argv);
+	return RUN_ALL_TESTS();
+}
+
+TEST(TestCaseName, TestName) {
+  EXPECT_EQ(10, 10);
+  EXPECT_TRUE(true);
+}
